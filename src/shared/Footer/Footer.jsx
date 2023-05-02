c23a7133cec020041872f8d5236477980a79d2db
Delowar Hossain <bscdelowar@yahoo.com>
2023-05-02 18:02:33 +0600

    Footer done

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import { FaPhone, FaRegEnvelope } from 'react-icons/fa'
-
+import './Footer.css'
 export const Footer = () => {
   return (
     <div className='' style={{backgroundColor:"#54595F", color:"#EFC40B"}}>
-      <Container>
-        <div className='contact'>
-          <h4>Contact Info.</h4>
-          <p><FaRegEnvelope></FaRegEnvelope>dhakacatering@gmail.com</p>
-          <p><FaPhone></FaPhone>00173811850</p>
+      <Container className='d-flex flex-column flex-md-row justify-content-around p-5 gap-5'>
+        <div className='contact w-100'>
+          <h4 className='my-4'>Contact Info.</h4>
+          <p className='d-flex align-items-center gap-2'><FaRegEnvelope className='fs-5'></FaRegEnvelope>dhakacatering@gmail.com</p>
+          <p className='d-flex align-items-center gap-2'><FaPhone className='fs-5'></FaPhone>00173811850</p>
         </div>
-        <div className='links'>
-          <h4>Important Links</h4>
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Services</li>
-            <li>Contact Us</li>
+        <div className='links w-100'>
+          <h4 className='my-4'>Important Links</h4>
+          <ul className='list-unstyled fs-5'>
+            <li className='my-2'>Home</li>
+            <li className='my-2'>About Us</li>
+            <li className='my-2'>Services</li>
+            <li className='my-2'>Contact Us</li>
           </ul>
         </div>
-        <div className='newsletters'>
-          <h4>Newsletter</h4>
+        <div className='newsletters flex-grow-1 w-100'>
+          <h4 className='my-4'>Newsletter</h4>
           <form>
-          
+            <div>
+              <input className='bgInput border-0 p-1 my-1' type="text" placeholder='Name'></input><br/>
+              <input className='bgInput border-0 p-1 my-1' type="text" placeholder='Email'></input><br/>
+              <input className='bgInput border-0 p-1 my-1' type="text" placeholder='Mobile'></input><br/>
+              <button className='btn btn-success w-100 my-1 text-warning fw-bold'>Subscirbe</button>
+            </div>
           </form>
         </div>
       </Container>
